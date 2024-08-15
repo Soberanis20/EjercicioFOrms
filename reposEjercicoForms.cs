2a5ec004e10635238fc6ac83ab90a01766b3b8af
Soberanis20 <javier.soberanis26@gmail.com>
2024-08-14 19:23:58 -0600

    Add files via upload

---
+++ reposEjercicoForms.cs
@@ -0,0 +1,46 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+              
+                if (string.IsNullOrWhiteSpace(TextBoxNombre.Text) ||
+                    string.IsNullOrWhiteSpace(TextBoxCorreo.Text) ||
+                    string.IsNullOrWhiteSpace(TextBoxMensaje.Text))
+                {
+                    
+                    LabelEstado.Text = "Por favor, complete todos los campos.";
+                    LabelEstado.ForeColor = Color.Red;
+                }
+                else
+                {
+                    
+                    LabelEstado.Text = "Formulario enviado correctamente.";
+                    LabelEstado.ForeColor = Color.Green;
+
+                    // Limpiar campos
+                    TextBoxNombre.Clear();
+                    TextBoxCorreo.Clear();
+                    TextBoxMensaje.Clear();
+                }
+            }
+
+        }
+    }
